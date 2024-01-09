2c086fb55f336da02e162435df382b16e43426c3
MikalJohnson <Johnsonmikal24@gmail.com>
2024-01-08 20:13:25 -0800

    First Commit

--- GuidedAssignment/Program.cs
+++ GuidedAssignment/Program.cs
@@ -9,7 +9,8 @@
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, You!");
+
             
         }
     }
